95f482c2ee5a7c2d0a14c063aa4ff619474afa05
Michael <biznesowymailkrifixa@gmail.com>
2023-03-16 01:13:32 +0100

    Feat: Finish phone view design for contact page

--- frontend/src/components/pages/ContactPage.tsx
+++ frontend/src/components/pages/ContactPage.tsx
@@ -5,6 +5,36 @@ import { SectionTitle } from "../UI/SectionTitle";
 import { SubmitButton } from "../UI/SubmitButton";
 import { TextArea } from "../UI/TextArea";
 
+const icons = [
+	{
+		name: "email",
+		icon: "../src/assets/images/email.svg",
+	},
+	{
+		name: "discord",
+		icon: "../src/assets/images/discord.svg",
+	},
+	{
+		name: "github",
+		icon: "../src/assets/images/github.svg",
+	},
+];
+
+const description: { [key: string]: { title: string; text: string } } = {
+	email: {
+		title: "Email",
+		text: "michalmuller\u200B@onet.eu",
+	},
+	discord: {
+		title: "Discord",
+		text: "KrifiZ#6922",
+	},
+	github: {
+		title: "Github",
+		text: "KrifiZ",
+	},
+};
+
 const ContactPage = () => {
 	return (
 		<>
@@ -12,7 +42,7 @@ const ContactPage = () => {
 				<h2 className="font-lato text-5xl uppercase text-white">Contact</h2>
 			</Hero>
 			<SectionTitle title="Get in touch" />
-			<div className="flex flex-grow flex-col justify-center  ">
+			<div className="flex flex-grow flex-col justify-center gap-2 p-2  ">
 				<Card>
 					<form className="p-1">
 						<Input
@@ -35,6 +65,22 @@ const ContactPage = () => {
 						<SubmitButton to={""} text={"Send Message"} size={"w-full"} />
 					</form>
 				</Card>
+				{icons.map((icon, index) => (
+					<div className="flex items-center">
+						<Card
+							className="max-h-[127px] min-h-[127px] min-w-[127px] max-w-[127px]"
+							key={index}
+						>
+							<img src={icon.icon} alt={icon.name} />
+						</Card>
+						<div className="flex flex-col justify-start break-words">
+							<h3 className=" text-3xl uppercase">
+								{description[icon.name].title}
+							</h3>
+							<p className="h-full  text-2xl ">{description[icon.name].text}</p>
+						</div>
+					</div>
+				))}
 			</div>
 		</>
 	);
