6b77d88df88dbeea32fcefd2d93a96f6ca2ec90e
Michael <biznesowymailkrifixa@gmail.com>
2023-03-26 23:58:52 +0200

    Feat: Add ModalButton

--- frontend/src/components/UI/Button.tsx
+++ frontend/src/components/UI/Button.tsx
@@ -10,7 +10,6 @@ const Button = (props: ButtonProps) => {
 	const { text, size, to } = props;
 
 	return (
-		// TODO add hover animation
 		<Link
 			className={`flex items-center justify-center rounded-xl bg-blue-500 p-2 text-white hover:bg-blue-400  hover:transition hover:delay-150 hover:duration-300 hover:ease-in-out ${size}`}
 			to={to}
